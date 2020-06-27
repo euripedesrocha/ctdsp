863bb1450bffdc38c0609e96c57578e5958fa904
Euripedes Rocha Filho <rocha.euripedes@gmail.com>
2020-06-27 19:33:39 +0100

    Revert "Merge from stash"
    This reverts commit f147e56fc28fed269fe390be39fcf41a65deb0ed.

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,6 @@
 #include <functional>
 #include <iterator>
 #include <numeric>
-#include <type_traits>
 
 #include "ctdsp.hpp"
 #include "windows.hpp"
@@ -22,34 +21,28 @@ bool all_equal_to(Data& data, double value) {
 }
 
 TEST_CASE("Boxcar window", "[window]") {
-  constexpr auto boxcar = ctdsp::array(std::integral_constant<size_t, 32>{},
-                                       ctdsp::window::boxcar{});
-
-  REQUIRE(all_equal(boxcar.data));
+  constexpr auto boxcar = ctdsp::window::boxcar<32>();
+  REQUIRE(all_equal(boxcar));
 }
 
-// TEST_CASE("Direct Window Creation", "[window]") {
-// using namespace ctdsp;
-// constexpr size_t length = 32;
-//// class gen {
-//// mutable double n = 0.0;
+TEST_CASE("Direct Window Creation", "[window]") {
+  using namespace ctdsp;
+  // Uses a immediatly invoked lambda to return the required data
+  [[maybe_unused]] constexpr auto manipulated_window = []() {
+    constexpr size_t length = 32;
+    constexpr auto generator = [n = 0]() mutable { return ++n; };
+    constexpr auto my_window = window::window<length>{generator};
+    return my_window;
+  }();
+}
 
-//// public:
-//// constexpr auto operator()() const noexcept { return n++; }
-////};
-//// constexpr auto my_window = ctdsp::make_array<length>(gen{});
-// constexpr auto my_window =
-// ctdsp::array{[n = 0.0]() mutable { return n++; }}.get();
-// REQUIRE(all_equal(my_window));
-//}
-
-// TEST_CASE("Window normalization", "ctdsp") {
-// using namespace ctdsp;
-// constexpr auto boxcar = window::boxcar<32>();
-// constexpr auto normalized = ctdsp::normalize(boxcar, by_value{5.0});
-// constexpr auto normalized_value = 1. / 5.0;
-// REQUIRE(all_equal_to(normalized, normalized_value));
-// constexpr auto max_normalized = normalize(boxcar, by_max{});
-// REQUIRE(all_equal_to(max_normalized, 1.0));
-//[[maybe_unused]] constexpr auto rms_normalized = normalize(boxcar, by_rms{});
-//}
+TEST_CASE("Window normalization", "ctdsp") {
+  using namespace ctdsp;
+  constexpr auto boxcar = window::boxcar<32>();
+  constexpr auto normalized = ctdsp::normalize(boxcar, by_value{5.0});
+  constexpr auto normalized_value = 1. / 5.0;
+  REQUIRE(all_equal_to(normalized, normalized_value));
+  constexpr auto max_normalized = normalize(boxcar, by_max{});
+  REQUIRE(all_equal_to(max_normalized, 1.0));
+  [[maybe_unused]] constexpr auto rms_normalized = normalize(boxcar, by_rms{});
+}
