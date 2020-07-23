84b03468e746806315c2e19964064b3ce10ae39e
Euripedes Rocha Filho <rocha.euripedes@gmail.com>
2020-07-23 07:45:20 +0100

    Adds some very basic tests on ctdsp::window
    - Just initial tests.

--- test/test_array.cpp
+++ test/test_array.cpp
@@ -3,19 +3,14 @@
 #include <type_traits>
 
 #include "ctdsp/array.hpp"
-
-template <class Data>
-bool all_equal(Data& data) {
-  return std::adjacent_find(data.cbegin(), data.cend(), std::not_equal_to{}) ==
-         data.cend();
-}
+#include "test_helpers.hpp"
 
 TEST_CASE("The user constructs the array and transform it", "[array]") {
   constexpr auto N = 32;
   constexpr auto arr =
       ctdsp::array<N>{} | [](auto& input) { std::ranges::fill(input, 1.); };
   STATIC_REQUIRE(std::is_same_v<decltype(arr), const ctdsp::array<N, double>>);
-  REQUIRE(all_equal(arr));
+  REQUIRE(test::all_equal(arr));
 }
 
 TEST_CASE("The user generate the array from a view", "[array]") {
--- test/test_window.cpp
+++ test/test_window.cpp
@@ -4,45 +4,17 @@
 #include <iterator>
 #include <numeric>
 
-#include "ctdsp/ctdsp.hpp"
-#include "ctdsp/windows.hpp"
+#include "ctdsp/window.hpp"
+#include "test_helpers.hpp"
 
-// template <class Data>
-// bool all_equal(Data& data) {
-// return std::adjacent_find(data.cbegin(), data.cend(), std::not_equal_to{}) ==
-// data.cend();
-//}
+TEST_CASE("A directly created Boxcar window") {
+  constexpr auto boxcar = ctdsp::window::boxcar<3>();
+  REQUIRE(test::all_equal(boxcar));
+}
 
-// template <class Data>
-// bool all_equal_to(Data& data, double value) {
-// auto target = Approx(value).epsilon(0.001);
-// return std::all_of(data.cbegin(), data.cend(),
-//[target](double item) { return item == target; });
-//}
+TEST_CASE("A directly created Bartlett window") {}
+TEST_CASE("Custom window creation using ctdsp::make") {
+  [[maybe_unused]] constexpr auto squared =
+      ctdsp::window::make<16>([](double n) { return n * n; });
+}
 
-// TEST_CASE("Boxcar window", "[window]") {
-// constexpr auto boxcar = ctdsp::window::boxcar<32>();
-// REQUIRE(all_equal(boxcar));
-//}
-
-// TEST_CASE("Direct Window Creation", "[window]") {
-// using namespace ctdsp;
-//// Uses a immediatly invoked lambda to return the required data
-//[[maybe_unused]] constexpr auto manipulated_window = []() {
-// constexpr size_t length = 32;
-// constexpr auto generator = [n = 0]() mutable { return ++n; };
-// constexpr auto my_window = window::window<length>{generator};
-// return my_window;
-//}();
-//}
-
-// TEST_CASE("Window normalization", "ctdsp") {
-// using namespace ctdsp;
-// constexpr auto boxcar = window::boxcar<32>();
-// constexpr auto normalized = ctdsp::normalize(boxcar, by_value{5.0});
-// constexpr auto normalized_value = 1. / 5.0;
-// REQUIRE(all_equal_to(normalized, normalized_value));
-// constexpr auto max_normalized = normalize(boxcar, by_max{});
-// REQUIRE(all_equal_to(max_normalized, 1.0));
-//[[maybe_unused]] constexpr auto rms_normalized = normalize(boxcar, by_rms{});
-//}
