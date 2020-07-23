e33927585f6bf95a46d8e7b39a7b54f80b4c82c8
Euripedes Rocha Filho <rocha.euripedes@gmail.com>
2020-07-23 07:42:35 +0100

    Changes the window generators to use ctdsp::array
    - The named functions are now templated lambda variables.
    - ctdsp::make uses ctdsp::array and depends on ranges.

--- src/ctdsp/window.hpp
+++ src/ctdsp/window.hpp
@@ -1,45 +1,30 @@
-#include <bits/c++config.h>
-
-#include <algorithm>
-#include <array>
 #include <cstddef>
-#include <cstdlib>
-#include <iterator>
+#include <ranges>
+#include <utility>
+
+#include "array.hpp"
 
 namespace ctdsp::window {
 
-template <std::size_t length>
-class window {
-  std::array<double, length> data{};
-  bool symmetric = false;
-  double correction_factor = 1.0;
+template <size_t length, class Generator, typename T = double>
+constexpr auto make(Generator &&generator) {
+  constexpr auto from = -(length / 2);
+  return std::ranges::views::iota(from) |
+         std::ranges::views::transform(std::forward<Generator>(generator)) |
+         ctdsp::to_array<length, T>;
+}
 
- public:
-  using value_type = typename decltype(data)::value_type;
-  template <class Generator>
-  consteval window(Generator generator) {
-    std::generate(data.begin(), data.end(), generator);
-  }
-  constexpr auto begin() noexcept { return data.begin(); }
-  constexpr auto end() noexcept { return data.end(); }
-  constexpr auto cbegin() const noexcept { return data.cbegin(); }
-  constexpr auto cend() const noexcept { return data.cend(); }
-  constexpr auto size() const noexcept { return data.size(); }
+template <size_t length, typename T = double>
+constexpr inline auto boxcar = []() {
+  auto generator = []([[maybe_unused]] auto n) { return T{1}; };
+  return make<length>(generator);
 };
 
 template <size_t length, typename T = double>
-constexpr auto boxcar() {
-  auto generator = []() { return T{1}; };
-  return window<length>(generator);
-}
-
-template <size_t length, typename T = double>
-constexpr auto bartlett() {
-  size_t n = 0;
-  auto generator = [n, half_length = (T{length} / T{2.0})]() mutable {
-    auto const value = 1 - std::abs((n - half_length) / half_length);
-    ++n;
+constexpr inline auto bartlett = []() {
+  auto generator = [](auto n) {
+    return T{1} - std::abs((n - (T{length} / T{2})) / (T{length} / T{2}));
   };
-  return window<length>(generator);
-}
+  return make<length>(generator);
+};
 }  // namespace ctdsp::window
